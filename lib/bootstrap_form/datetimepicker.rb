81af77dc0d219f1534f7a945972a14073f4df60b
Masato Ikeda <masato.ikeda@gmail.com>
2014-07-05 14:51:41 +0900

    Add dependency on bootstrap_form

--- lib/bootstrap_form/datetimepicker.rb
+++ lib/bootstrap_form/datetimepicker.rb
@@ -1,7 +1,10 @@
 require "bootstrap_form/datetimepicker/version"
+require "bootstrap_form"
 
 module BootstrapForm
   module Datetimepicker
     # Your code goes here...
   end
+
+  FormBuilder.send :include, Datetimepicker
 end
