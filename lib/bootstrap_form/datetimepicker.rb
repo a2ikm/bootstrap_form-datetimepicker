c51d3a1b5b74dd976c74bd4952aaaed22b9a5915
Masato Ikeda <masato.ikeda@gmail.com>
2014-07-21 02:33:29 +0900

    Add datetimepicker_class option to specify wrapping div's class
    The Default value is "bootstrap_form-datetimepicker".

--- lib/bootstrap_form/datetimepicker.rb
+++ lib/bootstrap_form/datetimepicker.rb
@@ -4,6 +4,8 @@ require "bootstrap_form"
 module BootstrapForm
   module Datetimepicker
     def datetime_picker(method, options = {})
+      datetimepicker_class = options.delete(:datetimepicker_class) || "bootstrap_form-datetimepicker" 
+
       form_group_builder(method, options) do
         prepend_and_append_input(options) do
           input_tag = @template.text_field(object_name, method, options)
@@ -11,7 +13,7 @@ module BootstrapForm
               content_tag :span, "", :class => "glyphicon glyphicon-calendar"
             end
           content_tag :div, input_tag.concat(span_tag),
-            :class => "bootstrap_form-datetimepicker input-group date",
+            :class => "#{datetimepicker_class} input-group date",
             :data => {
               "date-format" => "YYYY-MM-DD HH:mm:ss"
             }
--- spec/bootstrap_form/datetimepicker_spec.rb
+++ spec/bootstrap_form/datetimepicker_spec.rb
@@ -40,4 +40,23 @@ describe BootstrapForm::Datetimepicker do
 
     expect(horizontal_builder.datetime_picker(:until)).to eq expected
   end
+
+  it "should render wrapping div with datetimepicker_class option" do
+    expected = <<-HTML.gsub(/ *\n */, "")
+<div class="form-group">
+  <label class="control-label col-sm-2" for="schedule_until">Until</label>
+  <div class="col-sm-10">
+    <div class="custom-datetimepicker-class input-group date" data-date-format="YYYY-MM-DD HH:mm:ss">
+      <input class="form-control" id="schedule_until" name="schedule[until]" type="text" />
+      <span class="input-group-addon">
+        <span class="glyphicon glyphicon-calendar">
+        </span>
+      </span>
+    </div>
+  </div>
+</div>
+    HTML
+
+    expect(horizontal_builder.datetime_picker(:until, :datetimepicker_class => "custom-datetimepicker-class")).to eq expected
+  end
 end
