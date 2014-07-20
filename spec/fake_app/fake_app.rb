160191f4f974d063e7990d07d6be4aad8ee8c8d3
Masato Ikeda <masato.ikeda@gmail.com>
2014-07-21 00:44:08 +0900

    Create rails app for testing

---
+++ spec/fake_app/fake_app.rb
@@ -0,0 +1,40 @@
+require "active_record"
+require "action_controller/railtie"
+require "action_view/railtie"
+
+# config
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+module FakeApp
+  class Application < Rails::Application
+    config.secret_token = "All I've got to do is to bring my board."
+    config.session_store :cookie_store, :key => "_fake_app_session"
+    config.active_support.deprecation = :log
+    config.eager_load = false
+  end
+end
+FakeApp::Application.initialize!
+
+# routes
+FakeApp::Application.routes.draw do
+  resources :schedules, :only => [:new]
+end
+
+# models
+class Schedule < ActiveRecord::Base
+end
+
+# helpers
+module ApplicationHelper; end
+
+# controllers
+class ApplicationController < ActionController::Base
+  self.append_view_path File.dirname(__FILE__)
+end
+
+# migrations
+class CreateAllTables < ActiveRecord::Migration
+  def self.up
+    create_table(:schedules) { |t| t.datetime :until }
+  end
+end
